b248efad879dbf8c329fe733ad71596fd12f53ce
AdrienCastelbou <acastelbou1@gmail.com>
2020-05-06 12:22:13 +0200

    navbar and bootstrap v1 ok

---
+++ app/controllers/coaches_controller.rb
@@ -0,0 +1,27 @@
+class CoachesController < ApplicationController
+  def index
+    @coaches = Coach.all
+  end
+  def show
+
+  end
+
+  def new
+
+  end
+
+  def edit
+
+  end
+
+  def create
+  end
+
+  def update
+
+  end
+
+  def destroy
+    
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   devise_for :coaches
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :coaches
+  resources :users
+  resources :programs
+  resources :spheres
+  root to: "coaches#index"
 end
