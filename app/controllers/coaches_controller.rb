eab0f9cf625a4885e23b6cb54179ed54b2b0cdb8
AdrienCastelbou <acastelbou1@gmail.com>
2020-05-13 10:49:58 +0200

    description and city ok

--- app/controllers/coaches_controller.rb
+++ app/controllers/coaches_controller.rb
@@ -1,4 +1,6 @@
 class CoachesController < ApplicationController
+  before_action :set_coach, only: [:show, :edit, :update, :destroy]
+
   def index
     @coaches = Coach.all
   end
@@ -15,13 +17,39 @@ class CoachesController < ApplicationController
   end
 
   def create
+
   end
 
   def update
-
+    if @coach.update(description: params[:description], city: coach_city)
+      redirect_to coach_path(@coach),  :notice => "Votre profil a bien été modifié."
+    else
+      redirect_back(fallback_location: coach_path(@coach))
+    end
   end
 
   def destroy
     
   end
+
+  private
+
+  def set_coach
+    @coach = Coach.find(params[:id])
+  end
+
+  def coach_city
+    city = params[:city]
+    if @coach.city.name.downcase == city.downcase
+      return @coach.city
+    else
+      City.all.each do |old_city|
+        if old_city.name.downcase == city.downcase
+          return old_city
+        end
+      end
+      return City.create(name: city.capitalize)
+    end
+  end
+
 end
