51b19d90cada8ef12e9f6a619deff5fb74871cf1
AdrienCastelbou <acastelbou1@gmail.com>
2020-05-15 20:38:00 +0200

    devise for all ok, alias current_member added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,15 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  devise_group :member, contains: [:coach, :user]
+
+  private
+
+  def pundit_user
+      # Make Pundit to use whichever Devise model [Coach, User] as the 'current_user'
+      # Just by using the offered helper method from Devise, 'devise_group' feature
+      current_member
+  end
   protected
 
   def configure_permitted_parameters
