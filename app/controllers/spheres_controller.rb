47729efbe07de31dd58382625888f88ee869e9dc
AdrienCastelbou <acastelbou1@gmail.com>
2020-05-24 17:06:55 +0200

    coach card ok

--- app/controllers/spheres_controller.rb
+++ app/controllers/spheres_controller.rb
@@ -1,9 +1,11 @@
 class SpheresController < ApplicationController
+  before_action :set_sphere, only: [:show, :edit, :update, :destroy]
+
   def index
     
   end
   def show
-
+    @coaches = @sphere.coaches
   end
 
   def new
@@ -23,5 +25,11 @@ class SpheresController < ApplicationController
 
   def destroy
     
+  end
+
+  private 
+
+  def set_sphere
+    @sphere = Sphere.find(params[:id])
   end
 end
